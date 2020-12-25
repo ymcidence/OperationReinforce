f44fa01837c671a5604018f516c0f1463fb78cc3
yumishen <ymcidence@gmail.com>
2020-12-25 16:37:03 +0800

    123123

---
+++ util/data/feat.py
@@ -0,0 +1,13 @@
+SLOT_FEAT_NAME = ['query_slot_impr',
+                  'query_slot_pl_impr',
+                  'query_total_rev',
+                  'query_total_rev_pl_slots',
+                  'srp_pl_rev']
+
+QUERY_FEAT_NAME = ['Num_Items_In_Recall',
+                   'Num_PL_In_Recall',
+                   'Num_OL_In_Recall',
+                   'Min_PL_Ad_Rate',
+                   'Max_PL_Ad_Rate',
+                   'Avg_PL_Ad_Rate',
+                   'Median_PL_Ad_Rate']
